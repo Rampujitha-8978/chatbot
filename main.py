8f90b4e67a06db009df8bc308ae664f45629d602
Ram pujitha <24pa1a12g3@vishnu.edu.in>
2025-12-29 21:40:22 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+
+from google import genai
+from flask import Flask, app,request,render_template
+client=genai.Client(api_key="AIzaSyD1WyHPpPe6rITaAVmiL7QjC5K223bwXvs")
+app=Flask(__name__)
+
+@app.route('/')
+
+def home():
+
+    return render_template('index.html')
+
+@app.route("/chat",methods=["POST"])
+
+def chat():
+
+    prompt=request.json["message"]
+
+    response=client.models.generate_content(
+
+        models="gemini-2.5-flash",
+
+        contents=prompt
+
+    )
+
+    return jsonify({"reply":response.text})
+
+app.run(port=8000)
